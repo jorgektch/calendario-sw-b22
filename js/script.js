781ba41e7ed2c23dcf120986f6843d61e7b3ba53
Jorge Ktch <jorgektch@gmail.com>
2024-02-15 21:17:22 -0500

    Done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,334 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth'
+      initialView: 'dayGridMonth',
+      events: [
+        {
+         "title": "Cumpleaños de JORGE JESUS QUISPE VILLAVERDE",
+         "start": "2024-12-18",
+         "end": "2024-12-18"
+        },
+        {
+         "title": "Cumpleaños de YAYIR FLABIO MATTOS HILARIO",
+         "start": "2024-04-04",
+         "end": "2024-04-04"
+        },
+        {
+         "title": "Cumpleaños de DANIEL DAVID LORENZO RAMOS",
+         "start": "2024-07-31",
+         "end": "2024-07-31"
+        },
+        {
+         "title": "Cumpleaños de RODRIGO JOSE ALVA SÁENZ",
+         "start": "2024-09-22",
+         "end": "2024-09-22"
+        },
+        {
+         "title": "Cumpleaños de RUTH CAMILA ROJAS CASTAÑEDA",
+         "start": "2024-09-15",
+         "end": "2024-09-15"
+        },
+        {
+         "title": "Cumpleaños de JEREMY ANDREW ROSILLO RAMÍREZ",
+         "start": "2024-12-11",
+         "end": "2024-12-11"
+        },
+        {
+         "title": "Cumpleaños de JESUS STEVAN DIAZ INGOL",
+         "start": "2024-03-27",
+         "end": "2024-03-27"
+        },
+        {
+         "title": "Cumpleaños de ENMANUEL JOSE OBANDO SALINAS",
+         "start": "2024-07-19",
+         "end": "2024-07-19"
+        },
+        {
+         "title": "Cumpleaños de HUGO SEBASTIAN ALVAREZ MORA",
+         "start": "2024-10-29",
+         "end": "2024-10-29"
+        },
+        {
+         "title": "Cumpleaños de ALFREDO JHONATAN BARTOLO HUNGARO",
+         "start": "2024-06-21",
+         "end": "2024-06-21"
+        },
+        {
+         "title": "Cumpleaños de MATEO ALEJANDRO CHOY RIOS",
+         "start": "2024-05-22",
+         "end": "2024-05-22"
+        },
+        {
+         "title": "Cumpleaños de SOFIA FERNANDA SARMIENTO QUISPE",
+         "start": "2024-02-21",
+         "end": "2024-02-21"
+        },
+        {
+         "title": "Cumpleaños de ABRAHAM JOSUE CARBAJAL GUTIERREZ",
+         "start": "2024-01-03",
+         "end": "2024-01-03"
+        },
+        {
+         "title": "Cumpleaños de PAOLA RAYZA ABAL CARHUANCHO",
+         "start": "2024-11-14",
+         "end": "2024-11-14"
+        },
+        {
+         "title": "Cumpleaños de JOSE LUIS LA TORRE ROMERO",
+         "start": "2024-05-28",
+         "end": "2024-05-28"
+        },
+        {
+         "title": "Cumpleaños de JOAR JESUS ARANDA VEGA",
+         "start": "2024-10-01",
+         "end": "2024-10-01"
+        },
+        {
+         "title": "Cumpleaños de LAURA CECILIA MENDOZA MORALES",
+         "start": "2024-05-16",
+         "end": "2024-05-16"
+        },
+        {
+         "title": "Cumpleaños de ABIGAIL YOMELA LLANA OSORIO",
+         "start": "2024-06-10",
+         "end": "2024-06-10"
+        },
+        {
+         "title": "Cumpleaños de SHAMIR MANTILLA FLORES",
+         "start": "2024-01-25",
+         "end": "2024-01-25"
+        },
+        {
+         "title": "Cumpleaños de ANGELA LUCIA JARA ESPINOZA",
+         "start": "2024-03-30",
+         "end": "2024-03-30"
+        },
+        {
+         "title": "Cumpleaños de WILSON FABRIZZIO ESPINOZA APONTE",
+         "start": "2024-07-28",
+         "end": "2024-07-28"
+        },
+        {
+         "title": "Cumpleaños de DIEGO ALONSO CALDERON MATIAS",
+         "start": "2024-04-03",
+         "end": "2024-04-03"
+        },
+        {
+         "title": "Cumpleaños de VILBERTO ALBERTO PATRICIO JULCA",
+         "start": "2024-11-04",
+         "end": "2024-11-04"
+        },
+        {
+         "title": "Cumpleaños de FRANK KEVIN CONDOR HUARHUACHI",
+         "start": "2024-01-05",
+         "end": "2024-01-05"
+        },
+        {
+         "title": "Cumpleaños de BRAULIO ANDRE SALDAÑA ALARCON",
+         "start": "2024-03-21",
+         "end": "2024-03-21"
+        },
+        {
+         "title": "Cumpleaños de JHON FRANCO RIDOUTT QUIÑONES",
+         "start": "2024-07-14",
+         "end": "2024-07-14"
+        },
+        {
+         "title": "Cumpleaños de HARUMI NAYELI CONTRERAS QUISPE",
+         "start": "2024-09-08",
+         "end": "2024-09-08"
+        },
+        {
+         "title": "Cumpleaños de LEONARDO GABRIEL SEGURA PACHERRES",
+         "start": "2024-03-14",
+         "end": "2024-03-14"
+        },
+        {
+         "title": "Cumpleaños de JOSE BENJAMIN FLORES GOMEZ",
+         "start": "2024-02-02",
+         "end": "2024-02-02"
+        },
+        {
+         "title": "Cumpleaños de GIOVANNI ALEXANDER ARIAS CHUMPITAZ",
+         "start": "2024-10-06",
+         "end": "2024-10-06"
+        },
+        {
+         "title": "Cumpleaños de EDGARDO JUAN JULIAN BARBOZA RINCON",
+         "start": "2024-02-22",
+         "end": "2024-02-22"
+        },
+        {
+         "title": "Cumpleaños de JHAN BRAYAN ZAMIR REYES FABIAN",
+         "start": "2024-07-19",
+         "end": "2024-07-19"
+        },
+        {
+         "title": "Cumpleaños de JOSHÚA ANDRÉ BARRANTES NAVARRO",
+         "start": "2024-02-14",
+         "end": "2024-02-14"
+        },
+        {
+         "title": "Cumpleaños de BRUNO GIANCARLO FLORES ALCARRAZ",
+         "start": "2024-05-14",
+         "end": "2024-05-14"
+        },
+        {
+         "title": "Cumpleaños de GABRIEL OMAR USCAMAYTA SANCHEZ",
+         "start": "2024-04-18",
+         "end": "2024-04-18"
+        },
+        {
+         "title": "Cumpleaños de BRUNO OMAR CHOCHOCA YARLEQUE",
+         "start": "2024-12-06",
+         "end": "2024-12-06"
+        },
+        {
+         "title": "Cumpleaños de DARWIN JOSTEIN ROJAS ROJAS",
+         "start": "2024-08-15",
+         "end": "2024-08-15"
+        },
+        {
+         "title": "Cumpleaños de ERICK MARTIN ESTRADA BRICEÑO",
+         "start": "2024-11-17",
+         "end": "2024-11-17"
+        },
+        {
+         "title": "Cumpleaños de JOSE LUIS LIMACHI SARMIENTO",
+         "start": "2024-04-28",
+         "end": "2024-04-28"
+        },
+        {
+         "title": "Cumpleaños de KENNER ALISON TATAJE GUZMAN",
+         "start": "2024-02-17",
+         "end": "2024-02-17"
+        },
+        {
+         "title": "Cumpleaños de FAVIO ROUILLON HARO",
+         "start": "2024-12-28",
+         "end": "2024-12-28"
+        },
+        {
+         "title": "Cumpleaños de NICOLAS JEAN CARLOS AVALOS RODRIGUEZ",
+         "start": "2024-04-13",
+         "end": "2024-04-13"
+        },
+        {
+         "title": "Cumpleaños de JOSE LUIS CHACO FLORES",
+         "start": "2024-04-01",
+         "end": "2024-04-01"
+        },
+        {
+         "title": "Cumpleaños de MATIAS SEBASTIAN VINCULA NUÑOVERO",
+         "start": "2024-05-23",
+         "end": "2024-05-23"
+        },
+        {
+         "title": "Cumpleaños de MAXIMO LENNY ROMAN HURTADO",
+         "start": "2024-09-13",
+         "end": "2024-09-13"
+        },
+        {
+         "title": "Cumpleaños de FRANS ANDRE MALLCO LAURENTE",
+         "start": "2024-10-27",
+         "end": "2024-10-27"
+        },
+        {
+         "title": "Cumpleaños de RODRIGO DAVALOS BENITO",
+         "start": "2024-05-21",
+         "end": "2024-05-21"
+        },
+        {
+         "title": "Cumpleaños de JOSE LUIS CHAVEZ GAVE",
+         "start": "2024-06-22",
+         "end": "2024-06-22"
+        },
+        {
+         "title": "Cumpleaños de CHRISTIAN PIERO GUEVARA LAIZA",
+         "start": "2024-03-06",
+         "end": "2024-03-06"
+        },
+        {
+         "title": "Cumpleaños de ASTHRI JOANNE PARDAVE JARA",
+         "start": "2024-05-05",
+         "end": "2024-05-05"
+        },
+        {
+         "title": "Cumpleaños de LEONARDO JUSTO PANTA LABAN",
+         "start": "2024-08-04",
+         "end": "2024-08-04"
+        },
+        {
+         "title": "Cumpleaños de FABRIZIO GABRIEL RAMIREZ PATIÑO",
+         "start": "2024-05-20",
+         "end": "2024-05-20"
+        },
+        {
+         "title": "Cumpleaños de YAEL JORGE CHAVEZ BARDALES",
+         "start": "2024-12-26",
+         "end": "2024-12-26"
+        },
+        {
+         "title": "Cumpleaños de FLAVIO MANUEL SANTOS CHOZO",
+         "start": "2024-07-24",
+         "end": "2024-07-24"
+        },
+        {
+         "title": "Cumpleaños de MARCO ANTONIO CHUQUIYAURI GARCIA",
+         "start": "2024-09-24",
+         "end": "2024-09-24"
+        },
+        {
+         "title": "Cumpleaños de LUIS ANTONIO CAYCHO MUÑOZ",
+         "start": "2024-09-23",
+         "end": "2024-09-23"
+        },
+        {
+         "title": "Cumpleaños de KEVIN MARCELO JARA MEJIA",
+         "start": "2024-08-16",
+         "end": "2024-08-16"
+        },
+        {
+         "title": "Cumpleaños de YAN LUIS VILLANUEVA OLASCUAGA",
+         "start": "2024-02-20",
+         "end": "2024-02-20"
+        },
+        {
+         "title": "Cumpleaños de MANUEL ANDRE CHUMPITAZI RUIZ",
+         "start": "2024-12-26",
+         "end": "2024-12-26"
+        },
+        {
+         "title": "Cumpleaños de ELBERTH CESAR ZARAGOZA TOLENTINO",
+         "start": "2024-02-25",
+         "end": "2024-02-25"
+        },
+        {
+         "title": "Cumpleaños de DAYANA EUNICE CANCHO RAMIREZ",
+         "start": "2024-05-03",
+         "end": "2024-05-03"
+        },
+        {
+         "title": "Cumpleaños de PATRICK EFRAIN POSTILLOS ESTRADA",
+         "start": "2024-09-08",
+         "end": "2024-09-08"
+        },
+        {
+         "title": "Cumpleaños de LUIS DAVID JIMENEZ GUTIERREZ",
+         "start": "2024-09-11",
+         "end": "2024-09-11"
+        },
+        {
+         "title": "Cumpleaños de RODRIGO ALEXANDER CORNEJO WONG",
+         "start": "2024-02-27",
+         "end": "2024-02-27"
+        },
+        {
+         "title": "Cumpleaños de WILLS JURGEN PAJUELO BELLO",
+         "start": "2024-07-10",
+         "end": "2024-07-10"
+        }
+       ]
     });
     calendar.setOption('locale', 'es');
     calendar.render();
